acc509c7187134e91e87804e30d438420500f578
Romário Ferreira <romarioferreirarezende@hotmail.com>
2022-11-29 12:13:44 -0300

    pagina home no break point 320px

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,13 +1,11 @@
 import './assets/styles/custom.scss';
 import './App.css';
-import Navbar from './components/Navbar';
+import Home from 'pages/Home';
 
 const App = () => {
   return (
     <>
-      <Navbar/>
-      <h1>Hello Dscatalog</h1>
-
+      <Home />
     </>
   );
 };
---
+++ frontweb/src/pages/Home/index.tsx
@@ -0,0 +1,26 @@
+import Navbar from 'components/Navbar';
+import { ReactComponent as MainImage } from 'assets/images/main-image.svg';
+import './styles.css';
+import ButtonIcon from 'components/ButtonIcon';
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="home-container">
+        <div className="home-card">
+          <div className="home-content-container">
+            <h1>Conheça o Melhor catálogo de produtos!</h1>
+            <p> Ajudaremos você a encontrar os melhores produtos disponíveis no mercado</p>
+            <ButtonIcon/>
+          </div>
+          <div className="home-image-container">
+            <MainImage />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
