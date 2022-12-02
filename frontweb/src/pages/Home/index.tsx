65fec2f8391ab7cdbeab549c4c27fc2e80437451
Romário Ferreira <romarioferreirarezende@hotmail.com>
2022-12-02 12:28:27 -0300

    home style

--- frontweb/src/pages/Home/index.tsx
+++ frontweb/src/pages/Home/index.tsx
@@ -10,9 +10,16 @@ const Home = () => {
       <div className="home-container">
         <div className="home-card">
           <div className="home-content-container">
-            <h1>Conheça o Melhor catálogo de produtos!</h1>
-            <p> Ajudaremos você a encontrar os melhores produtos disponíveis no mercado</p>
-            <ButtonIcon/>
+            <div>
+              <h1>Conheça o Melhor catálogo de produtos!</h1>
+              <p>
+                {' '}
+                Ajudaremos você a encontrar os melhores produtos disponíveis no
+                mercado
+              </p>
+            </div>
+
+            <ButtonIcon />
           </div>
           <div className="home-image-container">
             <MainImage />
