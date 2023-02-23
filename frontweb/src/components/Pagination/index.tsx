449f59d102ad113b8230dec896a223d1fd4f7d2c
Romário Ferreira <romarioferreirarezende@hotmail.com>
2023-02-22 22:53:46 -0300

    pagination, styles

--- frontweb/src/components/Pagination/index.tsx
+++ frontweb/src/components/Pagination/index.tsx
@@ -1,16 +1,17 @@
 import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
+import './styles.css';
 
 const Pagination = () => {
   return (
     <div className="pagination-container">
-      <ArrowIcon />
-      <div className="pagination-item">1</div>
+      <ArrowIcon className='arrow-inactive arrow-previous' />
+      <div className="pagination-item active">1</div>
       <div className="pagination-item">2</div>
       <div className="pagination-item">3</div>
       <div className="pagination-item">4</div>
       <div className="pagination-item">...</div>
       <div className="pagination-item">10</div>
-      <ArrowIcon />
+      <ArrowIcon className='arrow-active' />
     </div>
   );
 };
