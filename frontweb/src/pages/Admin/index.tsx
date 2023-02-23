f628ad85cf43e711f196e263d5103526f9fdbd23
Romário Ferreira <romarioferreirarezende@hotmail.com>
2023-02-22 23:13:23 -0300

    admin layout

--- frontweb/src/pages/Admin/index.tsx
+++ frontweb/src/pages/Admin/index.tsx
@@ -1,11 +1,13 @@
-import Navbar from "./Navbar";
+import Navbar from './Navbar';
+import './styles.css';
 
 const Admin = () => {
   return (
     <div className="admin-container">
-      <Navbar/>
-      <div className="admin-content"></div>
-      <h1>conteudo</h1>
+      <Navbar />
+      <div className="admin-content">
+        <h1>conteudo</h1>
+      </div>
     </div>
   );
 };
