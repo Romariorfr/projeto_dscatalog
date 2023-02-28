6c805fb2c6e36c7bef598d8467b6231a204e4935
Romário Ferreira <romarioferreirarezende@hotmail.com>
2023-02-28 17:43:02 -0300

    axios with params

--- frontweb/src/pages/Catalog/index.tsx
+++ frontweb/src/pages/Catalog/index.tsx
@@ -1,76 +1,50 @@
 import ProductCard from 'components/ProductCard';
 import { Product } from 'types/product';
 import { Link } from 'react-router-dom';
-import './styles.css';
 import Pagination from 'components/Pagination';
+import { useEffect, useState } from 'react';
+import { SpringPage } from 'types/vendor/spring';
+import axios from 'axios';
+import { AxiosParams } from 'types/vendor/axios';
+import { BASE_URL } from 'util/request';
+
+import './styles.css';
 
 const Catalog = () => {
-  const product: Product = {
-    id: 2,
-    name: 'Smart TV',
-    price: 2190.0,
-    imgUrl:
-      'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg',
-    date: '2020-07-14T10:00:00Z',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    categories: [
-      {
-        id: 1,
-        name: 'Livros',
+  const [page, setPage] = useState<SpringPage<Product>>();
+
+  useEffect(() => {
+    const params: AxiosParams = {
+      method: 'GET',
+      url: `${BASE_URL}/products`,
+      params: {
+        page: 0,
+        size: 12,
       },
-      {
-        id: 3,
-        name: 'Computadores',
-      },
-    ],
-  };
+    };
+
+    axios(params).then((response) => {
+      setPage(response.data);
+    });
+  }, []);
 
   return (
     <div className="my-4 container catalog-container">
-
-      <div className='row catalog-title-container'>
+      <div className="row catalog-title-container">
         <h1>Catálogo de produtos</h1>
       </div>
 
       <div className="row">
         <div className="col-sm-6 col-lg-4 col-xl-3">
           <Link to="/products/1">
-            <ProductCard product={product} />
-          </Link>
-        </div>
-
-        <div className="col-sm-6 col-lg-4 col-xl-3">
-          <Link to="/products/1">
-            <ProductCard product={product} />
-          </Link>
-        </div>
-
-        <div className="col-sm-6 col-lg-4 col-xl-3">
-          <Link to="/products/1">
-            <ProductCard product={product} />
-          </Link>
-        </div>
-
-        <div className="col-sm-6 col-lg-4 col-xl-3">
-          <Link to="/products/1">
-            <ProductCard product={product} />
-          </Link>
-        </div>
-
-        <div className="col-sm-6 col-lg-4 col-xl-3">
-          <Link to="/products/1">
-            <ProductCard product={product} />
+            
           </Link>
         </div>
       </div>
 
-      <div className='row'>
-        <Pagination/>
+      <div className="row">
+        <Pagination />
       </div>
-
-
-
     </div>
   );
 };
