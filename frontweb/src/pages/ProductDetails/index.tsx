f301c42f68538acc5864cf7a3c86283a7e325c6c
Romário Ferreira <romarioferreirarezende@hotmail.com>
2023-02-28 17:11:33 -0300

    hook useParams

--- frontweb/src/pages/ProductDetails/index.tsx
+++ frontweb/src/pages/ProductDetails/index.tsx
@@ -1,21 +1,29 @@
 import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
 import axios from 'axios';
 import ProductPrice from 'components/Product Price';
+import { type } from 'os';
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { Product } from 'types/product';
 import { BASE_URL } from 'util/request';
 
 import './styles.css';
 
+type urlParams = {
+  productId: string;
+}
+
 const ProductDetails = () => {
+
+  const {productId} = useParams<urlParams>();
+  
   const [product, setProduct] = useState<Product>();
 
   useEffect(() => {
-    axios.get(BASE_URL + '/products/1').then((response) => {
+    axios.get(`${BASE_URL}/products/${productId}`).then((response) => {
       setProduct(response.data);
     });
-  }, []);
+  }, [productId]);
 
   return (
     <div className="product-details-container">
