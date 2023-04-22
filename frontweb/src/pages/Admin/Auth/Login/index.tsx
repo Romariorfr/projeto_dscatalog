e025ef8692b3bfbec16c6ddc2b7a82d2eac17526
Romário Ferreira <romarioferreirarezende@hotmail.com>
2023-04-22 20:39:46 -0300

    conditional styling

--- frontweb/src/pages/Admin/Auth/Login/index.tsx
+++ frontweb/src/pages/Admin/Auth/Login/index.tsx
@@ -49,7 +49,7 @@ const Login = () => {
               },
             })}
             type="text"
-            className="form-control base-input"
+            className={`form-control base-input ${errors.username ? 'is-invalid': ''}`}
             placeholder="Email"
             name="username"
           />
@@ -63,7 +63,7 @@ const Login = () => {
               required: 'Campo obrigatório',
             })}
             type="password"
-            className="form-control base-input "
+            className={`form-control base-input ${errors.password ? 'is-invalid': ''}`}
             placeholder="Password"
             name="password"
           />
