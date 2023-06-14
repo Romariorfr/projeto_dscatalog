d3dc0fb006169883e1d721b371c51ee7010ae09c
Romário Ferreira <romarioferreirarezende@hotmail.com>
2023-06-14 18:16:42 -0300

    :sparkles: feat: categoriyBadge

--- frontweb/src/pages/Admin/Products/List/index.tsx
+++ frontweb/src/pages/Admin/Products/List/index.tsx
@@ -1,4 +1,4 @@
-import ProductCrudCard from 'components/ProductCrudCard';
+import ProductCrudCard from 'pages/Admin/Products/ProductCrudCard';
 
 import './styles.css';
 import { Link } from 'react-router-dom';
@@ -15,6 +15,10 @@ const List = () => {
       {
         id: 2,
         name: 'Eletrônicos',
+      },
+      {
+        id: 1,
+        name: 'Computadores',
       },
     ],
     description:
