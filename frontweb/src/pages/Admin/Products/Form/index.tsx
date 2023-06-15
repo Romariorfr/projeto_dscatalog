7307db212bae67e5105f281726c8a9392898a5ca
Romário Ferreira <romarioferreirarezende@hotmail.com>
2023-06-15 10:05:12 -0300

    :lipstick: feat: form buttons styling

--- frontweb/src/pages/Admin/Products/Form/index.tsx
+++ frontweb/src/pages/Admin/Products/Form/index.tsx
@@ -6,7 +6,7 @@ const Form = () => {
       <div className="base-card product-crud-form-card">
         <h1 className="product-crud-form-title">DADOS DO PRODUTO</h1>
 
-        <form action=''>
+        <form action="">
           <div className="row product-crud-input-contaiener">
             <div className="col-lg-6 product-crud-inputs-left-container">
               <div className="margin-bottom-30">
@@ -20,12 +20,22 @@ const Form = () => {
               </div>
             </div>
             <div className="col-lg-6">
-              <textarea rows={10} className="base-input"></textarea>
+              <div>
+                <textarea
+                  rows={10}
+                  name=""
+                  className="form-control base-input h-auto"
+                />
+              </div>
             </div>
           </div>
-          <div className='product-crud-buttons-container'>
-            <button className="btn btn-outline-danger">CANCELAR</button>
-            <button className="btn btn-outline-primary">SALVAR</button>
+          <div className="product-crud-buttons-container">
+            <button className="btn btn-outline-danger product-crud-buttons ">
+              CANCELAR
+            </button>
+            <button className="btn btn-outline-primary product-crud-buttons">
+              SALVAR
+            </button>
           </div>
         </form>
       </div>
