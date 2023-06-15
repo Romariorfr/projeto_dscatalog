696221f5d0567fee9873ad371c7c68aaeefb64c1
Romário Ferreira <romarioferreirarezende@hotmail.com>
2023-06-15 10:47:12 -0300

    :sparkles: feat: start form integration

--- frontweb/src/pages/Admin/Products/Form/index.tsx
+++ frontweb/src/pages/Admin/Products/Form/index.tsx
@@ -1,22 +1,61 @@
+import { useForm } from 'react-hook-form';
 import './styles.css';
+import { Product } from 'types/product';
+import { AxiosRequestConfig } from 'axios';
+import { requestBackend } from 'util/requests';
 
 const Form = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Product>();
+
+  const onSubmit = (formData: Product) => {
+    const config: AxiosRequestConfig = {
+      method: 'POST',
+      url: '/products',
+      data: formData,
+      withCredentials: true,
+    };
+    requestBackend(config)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log('ERRO', error);
+      });
+  };
+
   return (
     <div className="product-crud-container">
       <div className="base-card product-crud-form-card">
         <h1 className="product-crud-form-title">DADOS DO PRODUTO</h1>
 
-        <form action="">
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="row product-crud-input-contaiener">
             <div className="col-lg-6 product-crud-inputs-left-container">
               <div className="margin-bottom-30">
-                <input type="text" className="form-control base-input" />
+                <input
+                  {...register('name', {
+                    required: 'Campo obrigatório',
+                  })}
+                  type="text"
+                  className={`form-control base-input ${
+                    errors.name ? 'is-invalid' : ''
+                  }`}
+                  placeholder="Nome do produto"
+                  name="name"
+                />
+                <div className="invalid-feedback d-block">
+                  {errors.name?.message}
+                </div>
               </div>
               <div className="margin-bottom-30">
                 <input type="text" className="form-control base-input" />
               </div>
               <div className="product-crud-input">
-                <input type="text" className="form-control base-input" /> 
+                <input type="text" className="form-control base-input" />
               </div>
             </div>
             <div className="col-lg-6">
