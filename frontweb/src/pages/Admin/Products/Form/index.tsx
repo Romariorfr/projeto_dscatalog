184f1281af51a8ef7439dadb51a43999729d383c
Romário Ferreira <romarioferreirarezende@hotmail.com>
2023-06-14 22:49:09 -0300

    :sparkles: feat: styling margins and containers for forms

--- frontweb/src/pages/Admin/Products/Form/index.tsx
+++ frontweb/src/pages/Admin/Products/Form/index.tsx
@@ -1,32 +1,36 @@
 import './styles.css';
 
 const Form = () => {
-  return(
-    <div className='product-crud-container'>
+  return (
+    <div className="product-crud-container">
       <div className="base-card product-crud-form-card">
-      <h1 className='product-crud-form-title'>DADOS DO PRODUTO</h1>
-      <form>
-        <div className='row'>
-          <div className='col-lg-6'>
-            <input type='text' className='form-control base-input'/>
-            <input type='text' className='form-control base-input'/>
-            <input type='text' className='form-control base-input'/>
+        <h1 className="product-crud-form-title">DADOS DO PRODUTO</h1>
 
+        <form action=''>
+          <div className="row product-crud-input-contaiener">
+            <div className="col-lg-6 product-crud-inputs-left-container">
+              <div className="margin-bottom-30">
+                <input type="text" className="form-control base-input" />
+              </div>
+              <div className="margin-bottom-30">
+                <input type="text" className="form-control base-input" />
+              </div>
+              <div className="product-crud-input">
+                <input type="text" className="form-control base-input" />
+              </div>
+            </div>
+            <div className="col-lg-6">
+              <textarea rows={10} className="base-input"></textarea>
+            </div>
           </div>
-          <div className='col-lg-6'>
-            <textarea rows={10} className='base-input'></textarea>
+          <div className='product-crud-buttons-container'>
+            <button className="btn btn-outline-danger">CANCELAR</button>
+            <button className="btn btn-outline-primary">SALVAR</button>
           </div>
-        </div>
-        <div>
-          <button className='btn btn-outline-danger'>CANCELAR</button>
-          <button className='btn btn-outline-primary'>SALVAR</button>
-        </div>
-      </form>
+        </form>
       </div>
-
     </div>
-  )
+  );
 };
 
 export default Form;
- 
