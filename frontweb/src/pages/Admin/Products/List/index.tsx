39fb39348c2d9598aa656e52bef0605cc3fba935
Romário Ferreira <romarioferreirarezende@hotmail.com>
2023-06-18 15:05:23 -0300

    :sparkles: feat: delete product,observer pattern

--- frontweb/src/pages/Admin/Products/List/index.tsx
+++ frontweb/src/pages/Admin/Products/List/index.tsx
@@ -12,6 +12,10 @@ const List = () => {
   const [page, setPage] = useState<SpringPage<Product>>();
 
   useEffect(() => {
+    getProducts();
+  }, []);
+
+  const getProducts = () => {
     const config: AxiosRequestConfig = {
       method: 'GET',
       url: '/products',
@@ -24,7 +28,7 @@ const List = () => {
     requestBackend(config).then((response) => {
       setPage(response.data);
     });
-  }, []);
+  };
 
   return (
     <div className="product-crud-container">
@@ -40,7 +44,12 @@ const List = () => {
       <div className="row">
         {page?.content.map((product) => (
           <div key={product.id} className="col-sm-6 col-md-12">
-            <ProductCrudCard product={product} />
+            <ProductCrudCard
+              onDelete={() => {
+                getProducts();
+              }}
+              product={product}
+            />
           </div>
         ))}
       </div>
--- frontweb/src/pages/Admin/Products/ProductCrudCard/index.tsx
+++ frontweb/src/pages/Admin/Products/ProductCrudCard/index.tsx
@@ -9,9 +9,10 @@ import { requestBackend } from 'util/requests';
 
 type Props = {
   product: Product;
+  onDelete: Function;
 };
 
-const ProductCrudCard = ({ product }: Props) => {
+const ProductCrudCard = ({ product,onDelete }: Props) => {
   const handleDelete = (productId: number) => {
 
     if (!window.confirm("Tem certeza que deseja deletar?")){
@@ -24,7 +25,7 @@ const ProductCrudCard = ({ product }: Props) => {
     };
 
     requestBackend(config).then(() => {
-      console.log('deletado id: ' + productId);
+      onDelete();
     });
   };
   return (
