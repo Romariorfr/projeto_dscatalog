fd8a3b27be8e521834b2f48e95134495fe25815b
Romário Ferreira <romarioferreirarezende@hotmail.com>
2023-06-20 16:55:20 -0300

    :sparkles: feat: pagination layout

--- frontweb/src/components/Pagination/index.tsx
+++ frontweb/src/components/Pagination/index.tsx
@@ -5,22 +5,20 @@ import ReactPaginate from 'react-paginate';
 
 const Pagination = () => {
   return (
-    <>
-      <ReactPaginate
-        pageCount={10}
-        pageRangeDisplayed={3}
-        marginPagesDisplayed={1}
-      />
-      <div className="pagination-container">
-        <ArrowIcon className="arrow-previous arrow-inactive" />
-        <div className="pagination-item active">1</div>
-        <div className="pagination-item">2</div>
-        <div className="pagination-item">3</div>
-        <div className="pagination-item">...</div>
-        <div className="pagination-item">10</div>
-        <ArrowIcon className="arrow-next arrow-active" />
-      </div>
-    </>
+    <ReactPaginate
+      pageCount={10}
+      pageRangeDisplayed={3}
+      marginPagesDisplayed={1}
+      containerClassName="pagination-container"
+      pageLinkClassName="pagination-item"
+      breakClassName="pagination-item"
+      previousLabel={<ArrowIcon />}
+      nextLabel={<ArrowIcon />}
+      previousClassName="arrow-previous"
+      nextClassName="arrow-next"
+      activeLinkClassName="pagination-link-active"
+      disabledClassName="arrow-inactive"
+    />
   );
 };
 
