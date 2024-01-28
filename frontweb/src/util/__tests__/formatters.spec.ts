a4d35ff9bfc15922c132d50371f75fdb86e695f5
Romário Ferreira <romarioferreirarezende@hotmail.com>
2024-01-28 11:07:51 -0300

    feat: jest test

--- frontweb/src/util/__tests__/formatters.spec.ts
+++ frontweb/src/util/__tests__/formatters.spec.ts
@@ -1,9 +1,20 @@
 import { formatPrice } from 'util/formatters';
 
-test('formatPrice should format number pt-BR when given a number', () => {
-  //act
-  const result = formatPrice(10.1);
+describe('formatPrice for positive numbers', () => {
+  test('formatPrice should format number pt-BR when given a number', () => {
+    const result = formatPrice(10.1);
+    expect(result).toEqual('10,10');
+  });
 
-  //assert
-  expect(result).toEqual('10,10');
+  describe('formatPrice for non-positive numbers', () => {
+    test('formatPrice should format number pt-BR when given a number 0', () => {
+      const result = formatPrice(0);
+      expect(result).toEqual('0,00');
+    });
+
+    test('formatPrice should format number pt-BR when given a number -1', () => {
+      const result = formatPrice(-1);
+      expect(result).toEqual('-1,00');
+    });
+  });
 });
