b90fe5df2be1591bb1abf5eaf3166cfa4af70ec8
Romário Ferreira <romarioferreirarezende@hotmail.com>
2024-01-29 10:24:31 -0300

    mocking with spyOn

--- frontweb/src/util/__tests__/auth.spec.ts
+++ frontweb/src/util/__tests__/auth.spec.ts
@@ -1,8 +1,19 @@
 import { hasAnyRoles } from '../auth';
+import * as TokenModule from '../token';
 
 describe('hasAnyRole', () => {
   test('should return true when empty list', () => {
     const result = hasAnyRoles([]);
+    expect(result).toEqual(true);
+  });
+
+  test('should return true when user has given role', () => {
+    jest.spyOn(TokenModule, 'getTokenData').mockReturnValue({
+      exp: 0,
+      user_name: 'bob',
+      authorities: ['ROLE_OPERATOR', 'ROLE_ADMIN'],
+    });
+    const result = hasAnyRoles(['ROLE_ADMIN']);
     expect(result).toEqual(true);
   });
 });
