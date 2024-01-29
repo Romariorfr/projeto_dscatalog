1fb14c67f5a0893ba6a987cfc1302a51e4585cdd
Romário Ferreira <romarioferreirarezende@hotmail.com>
2024-01-29 10:31:37 -0300

    test another hasAnyRole cenario

--- frontweb/src/util/__tests__/auth.spec.ts
+++ frontweb/src/util/__tests__/auth.spec.ts
@@ -16,4 +16,14 @@ describe('hasAnyRole', () => {
     const result = hasAnyRoles(['ROLE_ADMIN']);
     expect(result).toEqual(true);
   });
+
+  test('should return false when user does not have given role', () => {
+    jest.spyOn(TokenModule, 'getTokenData').mockReturnValue({
+      exp: 0,
+      user_name: 'bob',
+      authorities: ['ROLE_OPERATOR'],
+    });
+    const result = hasAnyRoles(['ROLE_ADMIN']);
+    expect(result).toEqual(false);
+  });
 });
