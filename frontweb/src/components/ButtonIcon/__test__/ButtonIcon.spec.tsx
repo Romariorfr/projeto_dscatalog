852b4187aeb9a651b91fb47a7bdb3c2120c04a44
Romário Ferreira <romarioferreirarezende@hotmail.com>
2024-02-17 11:45:40 -0300

    data testid demostration

--- frontweb/src/components/ButtonIcon/__test__/ButtonIcon.spec.tsx
+++ frontweb/src/components/ButtonIcon/__test__/ButtonIcon.spec.tsx
@@ -5,4 +5,5 @@ test('ButtonIcon should render button with given text', () => {
   const text = 'fazer login';
   render(<ButtonIcon text={text} />);
   expect(screen.getByText(text)).toBeInTheDocument();
+  expect(screen.getByTestId('arrow')).toBeInTheDocument();
 });
--- frontweb/src/components/ButtonIcon/index.tsx
+++ frontweb/src/components/ButtonIcon/index.tsx
@@ -4,16 +4,16 @@ import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
 
 type Props = {
   text: string;
-}
+};
 
-const ButtonIcon = ({ text } : Props) => {
+const ButtonIcon = ({ text }: Props) => {
   return (
     <div className="btn-container">
       <button className="btn btn-primary">
         <h6>{text}</h6>
       </button>
       <div className="btn-icon-container">
-        <ArrowIcon />
+        <ArrowIcon data-testid="arrow" />
       </div>
     </div>
   );
